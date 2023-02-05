4eaabbb075a7d1d1dc361fd1f28260075de4e15c
Laurent Grimaldi <lgi@manastone.fr>
2023-02-05 22:38:03 +0100

    ajout de l'entête préliminaire

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,19 @@ function App({ Component, pageProps }: AppProps) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         />
+        <meta
+          name="description"
+          content="Créateur de ManaKit, Manastone est une communautée de développeur et d'acteur du web français."
+        />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="noindex, nofollow" />
+
+        <meta name="author" content="Laurent Grimaldi" />
+        <meta name="copyright" content="©Manastone | 2021 - 2023" />
+        <meta name="generator" content="ManaKit & Next.js"></meta>
       </Head>
       <div className="container">
         <Nav />
